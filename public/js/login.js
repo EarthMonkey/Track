a2ab03c96cb7c082f938733793b924f6def1d79c
SURE <794637366@qq.com>
2016-10-18 17:48:06 +0800

    首页

--- public/js/login.js
+++ public/js/login.js
@@ -26,22 +26,26 @@ function showDiv(elem_id) {
 
 /** homepage **/
 function showIntro(index) {
-    var point = document.getElementsByClassName("point")[index];
-    var box = document.getElementById("bubble");
-    var intro = ["这里记录了您每天的运动数据、睡眠时间及体重变化",
-                 "在这里您可以发起活动，也可以参与其他用户发起的运动",
-                 "这里有您燃烧掉的卡路里、抛物线降落的体重，以及每个有我陪伴的夜晚",
-                 "这里见证了所有人挑战自我的执着，瓜分了每个人半途而废的念想"];
 
-    box.getElementsByTagName("span")[0].innerHTML = intro[index];
+    var parent = document.getElementsByClassName("each_pic")[index];
+    var intro = ["在这里您可以发起活动，也可以参与其他用户发起的运动",
+        "这里记录了您每天的运动数据、睡眠时间及体重变化",
+        "这里有您燃烧掉的卡路里、抛物线降落的体重，以及每个有我陪伴的夜晚",
+        "这里见证了所有人挑战自我的执着，瓜分了每个人半途而废的念想"];
 
-    box.style.left = (point.offsetLeft - 120) + "px";
+    var div = document.createElement("div");
+    div.innerHTML = intro[index];
+    div.className = "intro_text_over";
 
-    if (index == 0 || index == 1) {
-        box.style.top = (point.offsetTop - 95) + "px";
-    } else {
-        box.style.top = (point.offsetTop - 115) + "px";
-    }
+    parent.style.opacity = "0.6";
+    parent.style.boxShadow = "0 0 10px #cad2f7";
+    parent.appendChild(div);
+}
 
-    showDiv("bubble");
+function hideIntro(index) {
+    var parent = document.getElementsByClassName("each_pic")[index];
+    var child = document.getElementsByClassName("intro_text_over")[0];
+    parent.removeChild(child);
+    parent.style.opacity = "1";
+    parent.style.boxShadow = "0 0 7px #d8d8d8";
 }
