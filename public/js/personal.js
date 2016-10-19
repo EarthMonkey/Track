2de603e7013a8f3469b9b9bdc7f50095ce488762
SURE <794637366@qq.com>
2016-10-19 17:56:22 +0800

    兴趣爱好

--- public/js/personal.js
+++ public/js/personal.js
@@ -2,18 +2,23 @@
  * Created by L.H.S on 2016/10/19.
  */
 
+window.onload = function () {
+    getHobbies();
+};
+
+// 更新个人信息
 function updateInfo() {
 
     var bar = document.getElementsByClassName("left_nav_person")[0];
     var inputs = bar.getElementsByTagName("input");
     var is = bar.getElementsByTagName("i");
 
-    if(is[0].style.display == "" || is[0].style.display == "none") {
+    if (is[0].style.display == "" || is[0].style.display == "none") {
 
         var old = new Array();
 
         for (var i = 0; i < inputs.length; i++) {
-            if (i==inputs.length - 1) {
+            if (i == inputs.length - 1) {
                 inputs[i].disabled = false;
             } else {
                 inputs[i].readOnly = false;
@@ -28,7 +33,7 @@ function updateInfo() {
 
         is[0].onclick = function () {
             for (var i = 0; i < inputs.length; i++) {
-                if(i == inputs.length - 1) {
+                if (i == inputs.length - 1) {
                     inputs[i].disabled = true;
                 } else {
                     inputs[i].readOnly = true;
@@ -44,7 +49,7 @@ function updateInfo() {
         is[1].onclick = function () {
             for (var i = 0; i < inputs.length; i++) {
                 inputs[i].value = old[i];
-                if(i == inputs.length - 1) {
+                if (i == inputs.length - 1) {
                     inputs[i].disabled = true;
                 } else {
                     inputs[i].readOnly = true;
@@ -57,5 +62,36 @@ function updateInfo() {
             $(is[1]).hide();
         };
     }
+}
 
+function getHobbies() {
+    var parent = document.getElementById("hobbies");
+    var copy = document.getElementById("hobby_copy");
+
+    var hob = ["桌球", "羽毛球", "篮球", "健身", "跑步", "电影", "网球", "音乐", "吃吃吃", "码代码"];
+
+    for (var i = 0; i < 10; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = copy.innerHTML;
+        div.style.display = "inline-block";
+        div.getElementsByClassName("hobby_lbl")[0].getElementsByTagName("div")[0].innerHTML = hob[i];
+
+        parent.appendChild(div);
+    }
+
+}
+
+// 修改兴趣
+function hobbyMod() {
+
+    var parent = document.getElementById("hobbies");
+    var lbls = document.getElementById("hobbies").getElementsByTagName("i");
+    for (var i = 0; i < lbls.length; i++) {
+        $(lbls[i]).show();
+
+        lbls[i].onclick = function () {
+            var node = this.parentNode.parentNode;
+            parent.removeChild(node);
+        }
+    }
 }
